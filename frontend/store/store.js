25c93829ade94e4bc3577ad90856e6531135a4f1
stellayinwong <stellayinwong@gmail.com>
2019-07-22 19:41:42 -0400

    Remove logger

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -6,10 +6,10 @@ import rootReducer from '../reducers/root_reducer';
 
 const middlewares = [thunk];
 
-// if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV !== 'production') {
     const { logger } =  require('redux-logger');
     middlewares.push(logger);
-// }
+}
 
 const configureStore = (preloadedState = {}) => {
     return createStore(rootReducer, preloadedState, applyMiddleware(...middlewares));
